c188ca6271bd75728419692191a55ae15e16f33b
ubuntu <mmskm911@gmail.com>
2018-10-26 12:45:31 -0400

    New files

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/meshari/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/meshari/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/meshari/catkin_ws/devel;/opt/ros/melodic" != "" else []
